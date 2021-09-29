6ccf3b65f0f19c036d94d91d9f0d83a3aa7ead76
Laur <valgehall@gmail.com>
2021-09-29 10:40:09 +0300

    Siiimuga üle vaadatud

--- src/views/CardsUI.vue
+++ src/views/CardsUI.vue
@@ -6,12 +6,13 @@
       <h2 class="titleH2">Õpi tundma riike ja lippe!</h2>
       <div class="CardsUI">
         <div class="formContainer">
-          <form action="">
+          <p class="titleH2">{{ message2 }}</p>
+          <form action=""><h1 class="titleH1"></h1>
             <fieldset class="fieldInput">
               <!-- <input class="form-input" type="text" v-model="player_name" placeholder="Sinu nimi">-->
               <!-- <button v-on:click="enterPlayerName" type="submit" class="form-submit">Start</button>-->
-              <input class="form-input" type="text" placeholder="Sinu nimi">
-              <button @click="()=>$router.push('game')" type="submit" class="form-submit">Start</button>
+              <input class="form-input" type="text" v-model="player_name" placeholder="Sinu nimi">
+              <button @click="checkName()" type="submit" class="form-submit">Lisa</button>
             </fieldset>
           </form>
         </div>
@@ -22,14 +23,15 @@
           <!--          <p class="addIdText">Sellise nimega kasutaja on juba andmebaasis olemas. Sisesta oma ID:</p>-->
           <p class="addIdText">{{ message }}</p>
 
+
           <div class="formContainer">
             <form action="">
               <fieldset class="fieldInput">
                 <!-- <input class="form-input" type="text" v-model="playerID" placeholder="Sinu ID">-->
-                <input class="form-input" type="text" :placeholder="[[ IdMessage ]]">
+                <input class="form-input" type="text" v-model="playerID" :placeholder="[[ IdMessage ]]">
 
                 <!-- <button v-on:click="enterPlayerID" type="submit" class="form-submit">Lisa ID</button>-->
-                <button type="submit" class="form-submit">Lisa ID</button>
+                <button type="submit" @click="insertButton" class="form-submit">Edasi</button>
 
               </fieldset>
             </form>
@@ -45,15 +47,15 @@
           <!--        <button v-on:click="joinBuddyGame" type="submit" class="buttons btn btn-round material-design"-->
           <!--                data-color="#f0e9d7">Sõbra mäng-->
           <!--        </button>-->
-          <button @click="addId" type="submit" class="buttons btn btn-round material-design"
+          <button @click="randomNewGame" type="submit" class="buttons btn btn-round material-design"
                   data-color="#f0e9d7">Uus mäng
           </button>
-          <button @click="generateNewGame" type="submit"
+          <button @click="createFriendGame" type="submit"
                   class="buttons btn btn-round material-design"
-                  data-color="#f0e9d7">Uus mäng sõbraga
+                  data-color="#f0e9d7">Loo mäng sõbraga
           </button>
           <button @click="playWithFriend()" type="submit" class="buttons btn btn-round material-design"
-                  data-color="#f0e9d7">Sõbra mäng
+                  data-color="#f0e9d7">Ühine mänguga
           </button>
         </div>
       </div>
@@ -75,151 +77,122 @@ export default {
     return {
       hideInput: false,
       message: "!",
-      IdMessage: ""
+      IdMessage: "",
+      player_name: '',
+      isUnique: '',
+      notUnique: '',
+      playerID: '',
+      gameType: '',
+      gameID: '',
+      isJoin: '',
+      buttonState: 'new',
+      message2: ''
     }
   },
   methods: {
-    addId: function () {
-      // this.hideInput = !this.hideInput
-      if (this.hideInput = false) {
-        this.hideInput = true
-      } else if (this.hideInput = true) {
-        this.hideInput = true
-      }
-      this.IdMessage = "Sinu ID";
-      this.message = "Sellise nimega kasutaja on juba andmebaasis olemas. Sisesta oma ID:";
-      // this.message = this.message.replace("Sisesta sõbra mängu ID", "Sellise nimega kasutaja on juba andmebaasis olemas. Sisesta oma ID:");
-    },
-    generateNewGame: function () {
-      if (this.hideInput = true) {
-        this.hideInput = false
-      } else if (this.hideInput = false) {
-        this.hideInput = false
-      }
+    randomNewGame: function () {
+      this.gameType = false
+      this.hideInput = false
+      this.$http.get("/checkgame/" + this.playerID + "/" + this.gameType)
+          .then(response => {
+            this.gameID = response.data
+            this.pollReady()
+          })
 
-      // if (this.hideInput = true) {
-      //   setTimeout(() => this.hideInput = false, 2000);
-      // } else if (this.hideInput = false) {
-      //   setTimeout(() => this.hideInput = false, 2000);
-      // }
+
     },
+    createFriendGame: function () {
+      this.gameType = true
+      this.$http.get("/creategame/" + this.playerID + "/" + this.gameType)
+          .then(response => {
+            console.log(response.data)
+            this.gameID = response.data
+            this.pollReady()
+          })
+
+    },
+
     playWithFriend: function () {
-      if (this.hideInput = false) {
-        this.hideInput = true
-      } else if (this.hideInput = true) {
-        this.hideInput = true
-        // setTimeout(() => this.hideInput = true, 500);
-
-      }
+      this.hideInput = false
+      this.buttonState = "join"
       this.IdMessage = "Mängu ID";
       this.message = "Sisesta sõbra mängu ID";
-      // this.message = this.message.replace("Sellise nimega kasutaja on juba andmebaasis olemas. Sisesta oma ID:", "Sisesta sõbra mängu ID");
+      this.hideInput = true
+
+    }
+    ,
+    checkName: function () {
+      console.log(this.buttonState)
+      this.buttonState = "new"
+      console.log(this.$http)
+      this.$http.get("/uniquename/" + this.player_name)
+          .then(response => {
+            this.hideInput = !(response.data)
+            if (response.data) {
+              this.$http.get("/insertname/" + this.player_name)
+                  .then(response => {
+                    this.playerID = response.data
+                  })
+            } else {
+
+              this.IdMessage = "Sinu ID";
+              this.message = "Sellise nimega kasutaja on juba andmebaasis olemas. Sisesta oma ID:";
+            }
+          })
+
+
+    },
+    insertButton: function () {
+      console.log(this.buttonState)
+      if (this.buttonState == "join") {
+        this.$http.get("/joingame/" + this.playerID + "/" + this.gameID)
+            .then(response => {
+              console.log(response.data)
+              this.pollReady()
+            })
+      } else {
+        this.$http.get("/checkNameId/" + this.player_name + "/" + this.playerID)
+            .then(response => {
+              if (response.data) {
+                this.message2 = "Vali nüüd altpoolt mängutüüp";
+              } else {
+                this.hideInput = true
+                this.message2 = "See ei ole õige ID! Sisesta mängijanimi!"
+              }
+            })
+      }
+    },
+
+    pollReady: function () {
+      if (this.gameID) {
+        this.$http.get('/checkready/' + this.gameID)
+            .then(response => {
+              if (response.data === true) {
+                console.log(response);
+                console.log("kontroll")
+                localStorage.playerName = this.player_name
+                localStorage.gameID = this.gameID
+                localStorage.playerID = this.playerID
+                this.$router.push('GeoheeGame')
+                clearInterval(this.pollInterval)
+              }
+              this.status = response;
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+      }
+    }
+  },
+  mounted() {
+    this.pollReady()
+    if (this.status !== true) {
+      this.pollInterval = setInterval(this.pollReady, 3000)
+      setTimeout(() => {
+        clearInterval(this.pollInterval)
+      }, 600000)
     }
   }
 }
+
 </script>
-
-<!--<script>-->
-<!--export default {-->
-<!--  name: 'addID',-->
-<!--  data: function () {-->
-<!--    return {-->
-<!--      active: true-->
-<!--    }-->
-<!--  }-->
-<!--}-->
-<!--</script>-->
-
-<!--<script>-->
-<!--export default {-->
-<!--  name: 'addID',-->
-<!--  data: function () {-->
-<!--    return {-->
-<!--      isUnique: false-->
-<!--    }-->
-<!--  }-->
-<!--}-->
-<!--</script>-->
-
-<!--<script>-->
-<!--export default {-->
-<!--  name: 'CardsUI',-->
-<!--  data: function () {-->
-<!--    return {-->
-<!--      visible: true-->
-<!--    }-->
-<!--  }-->
-<!--}-->
-<!--</script>-->
-
-
-<!--<script>-->
-<!--export default {-->
-<!--  name: 'CardsUI',-->
-<!--  data: function () {-->
-<!--    return {-->
-<!--      player_name: '',-->
-<!--      isUnique: '',-->
-<!--      notUnique: '',-->
-<!--      playerID: '',-->
-<!--      buddyGame: ''-->
-
-
-<!--    }-->
-<!--  },-->
-<!--  methods: {-->
-
-<!--    enterPlayerName: function () {-->
-<!--      this.$http.get("/uniquename/" + this.player_name)-->
-<!--          .then(response => {-->
-<!--            this.isUnique = !response.data //service tagastab true kui on unique-->
-<!--            if (response.data) {-->
-<!--              this.$http.get()("/insertname/" + this.player_name)-->
-<!--                  .then(response => {-->
-<!--                    this.playerID = response.data-->
-<!--                  })-->
-<!--            }-->
-<!--          })-->
-<!--    },-->
-<!--    enterPlayerID: function () {-->
-<!--      this.notUnique = true-->
-<!--    },-->
-<!--    findNewGame: function () {-->
-<!--      this.buddyGame = false-->
-<!--      this.$http.get("/checkgame/" + this.playerID + this.buddyGame)   //kui on olemas poolik mäng, siis lisab READY-->
-<!--          .then(response => {-->
-<!--            console.log(response.data)-->
-<!--            this.gameID = response.data-->
-<!--            this.$http.get("/updateplayer/" + this.playerID + this.gameID)    //kui-->
-<!--                .then(response => {-->
-<!--                  console.log(response.data)-->
-<!--                })-->
-<!--          })-->
-<!--    },-->
-<!--    createBuddyGame: function () {-->
-<!--      this.buddyGame = true-->
-<!--      this.$http.get("/creategame/" + this.playerID + this.buddyGame)-->
-<!--          .then(response => {-->
-<!--            console.log(response.data)-->
-<!--            this.$http.get("/updateplayer/" + this.playerID)-->
-<!--                .then(response => {-->
-<!--                  console.log(response.data)-->
-<!--                })-->
-<!--          })-->
-<!--    },-->
-<!--    joinBuddyGame: function () {-->
-<!--      this.buddyGame = true-->
-<!--      gameID-->
-<!--      this.$http.get("/joingame/" + this.player_name + this.gameID)-->
-<!--          .then(response => {-->
-<!--            console.log(response.data)-->
-<!--            this.$http.get("/updateplayer/" + this.playerID)-->
-<!--                .then(response => {-->
-<!--                  console.log(response.data)-->
-<!--                })-->
-<!--          })-->
-
-<!--    }-->
-<!--  }-->
-<!--}-->
-<!--</script>-->
